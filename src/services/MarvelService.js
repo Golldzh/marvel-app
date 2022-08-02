fe259eb4aba65b1a274a5288a625dbfcdf64be52
Gulzhan <bekshoinova.g@gmail.com>
2022-08-02 13:28:08 +0600

    update RandomChar component and worked with data transformation

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -7,8 +7,23 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(`${process.env.REACT_APP_MARVEL_API_BASE}/characters?limit=9&offset=210&${process.env.REACT_APP_MARVEL_API_KEY}`)
+  getAllCharacters = async() => {
+    const res = await this.getResource(`${process.env.REACT_APP_MARVEL_API_BASE}characters?limit=9&offset=210&${process.env.REACT_APP_MARVEL_API_KEY}`);
+    return res.data.results.map(this._transformCharacters);
+  }
+  getCharacter = async(id) => {
+    const res = await this.getResource(`${process.env.REACT_APP_MARVEL_API_BASE}characters/${id}?${process.env.REACT_APP_MARVEL_API_KEY}`);
+    return this._transformCharacters(res.data.results[0]);
+  }
+
+  _transformCharacters = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
   }
 }
 
